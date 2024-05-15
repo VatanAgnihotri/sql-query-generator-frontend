43e37621e8617d2f9bd004f4c281dde3bc2ddc31
Vatan Agnihotri <vatansharma07@gmail.com>
2024-05-16 02:01:23 +0530

    Result component

--- src/Components/Result.tsx
+++ src/Components/Result.tsx
@@ -1,28 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { ResultComponentProps } from "../Interfaces/Result.interface";
-
+import COPY_SVG from "../assets/copy.svg";
+import COPIED_SVG from "../assets/copied.svg";
 const Result: React.FC<ResultComponentProps> = (props) => {
+  const [copied, setCopied] = useState(false);
   return (
-    <div className="mt-10 sm:mt-20">
+    <div title="Click to copy text to clipboard." className="mt-10 sm:mt-20">
       <a
+        onClick={() => {
+          navigator.clipboard.writeText(props.query);
+          setCopied(true);
+        }}
         className="m-1 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
         href="#"
       >
-        <svg
-          className="flex-shrink-0 size-4"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path d="M17.8 19.2 16 11l3.5-3.5C21 6 21.5 4 21 3c-1-.5-3 0-4.5 1.5L13 8 4.8 6.2c-.5-.1-.9.1-1.1.5l-.3.5c-.2.5-.1 1 .3 1.3L9 12l-2 3H4l-1 1 3 2 2 3 1-1v-3l3-2 3.5 5.3c.3.4.8.5 1.3.3l.5-.2c.4-.3.6-.7.5-1.2z" />
-        </svg>
         {props?.query}
+        <img
+          src={copied ? COPIED_SVG : COPY_SVG}
+          alt="Please check your internet connection!"
+        />
       </a>
     </div>
   );
